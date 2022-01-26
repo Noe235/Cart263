89e029f2793f1462077c22c3b16c708984892766
Noe235 <sannoemie@outlook.com>
2022-01-26 00:38:24 -0500

    Added Fruits and game states

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -1,14 +1,241 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Slamina
+Noemie
+guess the animal name backward game
 */
 
 "use strict";
 
-
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
+];
+const fruits = [
+  "apple",
+  "apricot",
+  "avocado",
+  "banana",
+  "bell pepper",
+  "bilberry",
+  "blackberry",
+  "blackcurrant",
+  "blood orange",
+  "blueberry",
+  "boysenberry",
+  "breadfruit",
+  "canary melon",
+  "cantaloupe",
+  "cherimoya",
+  "cherry",
+  "chili pepper",
+  "clementine",
+  "cloudberry",
+  "coconut",
+  "cranberry",
+  "cucumber",
+  "currant",
+  "damson",
+  "date",
+  "dragonfruit",
+  "durian",
+  "eggplant",
+  "elderberry",
+  "feijoa",
+  "fig",
+  "goji berry",
+  "gooseberry",
+  "grape",
+  "grapefruit",
+  "guava",
+  "honeydew",
+  "huckleberry",
+  "jackfruit",
+  "jambul",
+  "jujube",
+  "kiwi fruit",
+  "kumquat",
+  "lemon",
+  "lime",
+  "loquat",
+  "lychee",
+  "mandarine",
+  "mango",
+  "mulberry",
+  "nectarine",
+  "nut",
+  "olive",
+  "orange",
+  "papaya",
+  "passionfruit",
+  "peach",
+  "pear",
+  "persimmon",
+  "physalis",
+  "pineapple",
+  "plum",
+  "pomegranate",
+  "pomelo",
+  "purple mangosteen",
+  "quince",
+  "raisin",
+  "rambutan",
+  "raspberry",
+  "redcurrant",
+  "rock melon",
+  "salal berry",
+  "satsuma",
+  "star fruit",
+  "strawberry",
+  "tamarillo",
+  "tangerine",
+  "tomato",
+  "ugli fruit",
+  "watermelon"
+];
+const praise = [
+  'Good job!',
+  'Nice!',
+  'Nice work',
+  'Congratulation',
+  'Perfect',
+  'felicitation',
+];
+let currentAnimal = 'no';
+let currentAnswer = '';
+let gamestate = 'menu' //menu, game, game over
+let game = 'none'
 /**
 Description of preload
 */
@@ -21,7 +248,21 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+
+  if (annyang) {
+    let commands = {
+      'I think it is *animal': guessAnimal
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+
+    textSize(32);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+
+  }
 }
 
 
@@ -29,5 +270,85 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  if (gamestate === 'menu') {
+    fill(255, 255, 255);
+    text('Click Right to guess fruit   Left to guess animals', width / 2, height / 2);
+    textSize(32);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+
+  } else if (gamestate === 'game') {
+    fill(255, 255, 255);
+
+    text('Click to play the sound', width / 2, 200);
+    textSize(32);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+
+    if (currentAnswer === currentAnimal) {
+      fill(0, 255, 0);
+
+    } else {
+      fill(255, 0, 0);
+    }
+    text(currentAnswer, width / 2, height / 2);
+
+  } else if (gamestate === 'over') {
+    text('Click to try again', width / 2, height / 2);
+  }
+}
+
+function guessAnimal(animal) {
+  currentAnswer = animal.toLowerCase();
+
+}
+
+function mousePressed() {
+
+  if (gamestate === 'menu') {
+    if (mouseX > width / 2) {
+      gamestate = 'game';
+      game = 'animals';
+    }
+    if (mouseX < width / 2) {
+      gamestate = 'game';
+      game = 'fruits';
+    }
+  }
+
+  if (gamestate === 'game' && game === 'animals') {
+    currentAnimal = random(animals);
+    let reverseAnimal = reverseString(currentAnimal);
+    responsiveVoice.speak(reverseAnimal);
+
+  }
+  if (gamestate === 'game' && game === 'fruits') {
+    currentAnimal = random(fruits);
+    let reverseFruit = reverseString(currentAnimal);
+    responsiveVoice.speak(reverseFruit);
+
+  }
+  if (gamestate === 'over')
+    gamestate = 'game';
+
+
+
+}
+
+
+/**
+Reverses the provided string
+From Pippin
+*/
+function reverseString(string) {
+  // Split the string into an array of characters
+  let characters = string.split('');
+  // Reverse the array of characters
+  let reverseCharacters = characters.reverse();
+  // Join the array of characters back into a string
+  let result = reverseCharacters.join('');
+  // Return the result
+  return result;
 }
