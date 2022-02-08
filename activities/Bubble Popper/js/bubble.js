1b54c9a539198c3bbb8b00ca23a4186851965d83
Noe235 <90280480+Noe235@users.noreply.github.com>
2022-02-08 16:23:30 -0500

    E3 working

---
+++ activities/Bubble Popper/js/bubble.js
@@ -0,0 +1,33 @@
+class Bubble{
+//constructor
+constructor(){
+this.x =random(width);
+this.y = height;
+this.size =100;
+this.vx =0;
+this.vy=-2;
+this.color ={
+  r:random(0,255),
+  g:random(0,255),
+  b:random(0,255),
+}
+};
+//display
+display(){
+push();
+fill(this.color.r,this.color.g,this.color.b);
+noStroke();
+ellipse (this.x, this.y,this.size);
+pop();
+
+}
+
+//movement bubble
+movement(){
+//moving the buble
+this.x += this.vx;
+this.y += this.vy;
+}
+
+
+}
--- activities/Bubble Popper/js/script.js
+++ activities/Bubble Popper/js/script.js
@@ -9,7 +9,7 @@ Pop bubble with your hand
 //Things to add
 //score => easy
 //sfx => easy
-//menu screen + loading screen =>medium-easy ish
+//menu screen + loading screen =>medium-easy ish depends maybe with oop again
 //more bubble => oop => prob harder
 
 
@@ -17,10 +17,13 @@ Pop bubble with your hand
 let video = undefined;
 
 //hand pose model
-let handpose  undefined;
+let handpose =undefined;
+let predictions=[];
 
 //Bubble
-let bubble= undefined;
+let bubble= undefined; // => s[];
+let nbbubble = 5
+
 
 /**
 Description of preload
@@ -48,17 +51,20 @@ function(){
 
 //listen for prediction
 handpose.on('predict',function(results){
-  console.log(results);
-  predictions= results;
+  predictions = results;
 });
 
+//can remove +oop
 bubble ={
   x:random(width),
   y:height,
-  siz:100,
+  size:100,
   vx:0,
   vy:-2,
 };
+
+//adding when oop ready
+//let bubble = new Bubble();
 
 }
 /**
@@ -66,31 +72,17 @@ Description of draw()
 */
 function draw() {
 background(0);
-if(prediciton.length > 0){
+if (predictions.length > 0){
 let hand = predictions [0];
-let index = hand.annotation.indexFinger;
+let index = hand.annotations.indexFinger;
 let tip = index[3];
 let base = index[0];
 let tipX = tip[0];
 let tipY = tip[1];
 let baseX= base[0];
 let baseY = base[1];
+ displayPin(baseX, baseY, tipX, tipY);
 
-//pin body
-push();
-noFill();
-stroke(255,255,255);
-strokeWeight(2);
-line(baseX, baseY, tipX, tipY);
-pop();
-
-
-//pin head
-push();
-noStroke();
-fill(255,0,0);
-ellipse(baseX,baseY,20);
-pop();
 
 //check bubble poppin
 let d = dist(tipX,tipY,bubble.x,bubble.y);
@@ -98,22 +90,68 @@ if (d< bubble.size/2){
   bubble.x = random(width);
   bubble.y = height;
 }
+//modified version check every bubble
+// for (let i = 0; i <bubbles.lenght; i++){
+//let bubble = bubbles[i];
+// let d = dist(tipX,tipY,bubble.x,bubble.y);
+// if (d< bubble.size/2){
+// bubbles.splice();
+//}
+//
+//
 }
 
-//moving the buble
+//making so that there is always 5 bubble on screen
+// for (let i = 0; i <nbbubble ; i++){
+//   let x =random(width);
+//   let y = height;
+//   let size =100;
+//   let vx =0;
+//   let vy=-2;
+//   let color ={
+//     r:random(0,255);
+//     g:random(0,255);
+//     b:random(0,255);
+// }
+// let bubble = new bubble (x,y,size,vx,vy,color);
+// bubble.push(bubble);
+
+
+//moving the buble + in oop
 bubble.x += bubble.vx;
 bubble.y += bubble.vy;
+// => bubble.movement()
 
+//will in theory be deleted since new bubble will be made
 if (bubble.y<0) {
   bubble.x = random(width);
   bubble.y = height;
 }
 
+//add to oop
 push();
-fill(0,0,255 );
+fill(0,0,255);
 noStroke();
-ellipse (bubble.x, bubble.y,bubble.size);
+ellipse(bubble.x,bubble.y,bubble.size);
 pop();
+//=> bubble.display()
+
+}
+
+function displayPin(baseX, baseY, tipX, tipY){
+  //pin body
+  push();
+  noFill();
+  stroke(255,255,255);
+  strokeWeight(2);
+  line(baseX, baseY, tipX, tipY);
+  pop();
 
 
+  //pin head
+  push();
+  noStroke();
+  fill(255,0,0);
+  ellipse(baseX,baseY,20);
+  pop();
 }
