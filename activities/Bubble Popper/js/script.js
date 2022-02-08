6ace95cc58ee2242dda030eedfcf5bb5fefb65ab
Noe235 <90280480+Noe235@users.noreply.github.com>
2022-02-08 17:16:46 -0500

    E3- game state done

--- activities/Bubble Popper/js/script.js
+++ activities/Bubble Popper/js/script.js
@@ -10,9 +10,12 @@ Pop bubble with your hand
 //score => easy
 //sfx => easy
 //menu screen + loading screen =>medium-easy ish depends maybe with oop again
-//more bubble => oop => prob harder
 
 
+//gamestate
+let gamestate = 'loading'
+let score = 0;
+let timer = 1200;
 //user webcam
 let video = undefined;
 
@@ -47,6 +50,7 @@ handpose = ml5.handpose(video,{
 },
 function(){
   console.log('model works');
+  gamestate = 'menu'
 });
 
 //listen for prediction
@@ -76,6 +80,29 @@ Description of draw()
 */
 function draw() {
 background(0);
+if (gamestate==='loading'){
+  background(255);
+  push();
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(`Loading...`, width / 2, height / 2);
+  pop();
+}
+
+if (gamestate==='menu'){
+  push();
+  fill(255,255,255);
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(`Pop as much ballons as possible`, width / 2, height / 2);
+  textSize(25);
+  text(`click to start`, width / 2, height / 2 +100);
+  pop();
+}
+
+if(gamestate=== 'game') {
 if (predictions.length > 0){
 let hand = predictions [0];
 let index = hand.annotations.indexFinger;
@@ -88,12 +115,6 @@ let baseY = base[1];
  displayPin(baseX, baseY, tipX, tipY);
 
 
-//check bubble poppin
-// let d = dist(tipX,tipY,bubble.x,bubble.y);
-// if (d< bubble.size/2){
-//   bubble.x = random(width);
-//   bubble.y = height;
-// }
 
 //modified version check every bubble
 for (let i = 0; i <bubbles.length; i++){
@@ -101,6 +122,7 @@ let bubble = bubbles[i];
 let d = dist(tipX,tipY,bubble.x,bubble.y);
 if (d< bubble.size/2){
 bubbles.splice(i,1);
+score+=1;
 }
 }
 
@@ -130,8 +152,33 @@ bubble.display();
 bubble.loop();
 }
 
+time();
+overlay();
+if (timer <1){
+  gamestate = 'gameover';
 }
+}
+}
+function time(){
+  if(timer >0){
+  timer -=1;
+}
+}
+function overlay(){
+  push();
+  fill(255,255,255);
+  textSize(12);
+  textAlign(LEFT);
+  text(`Score ${score}`, 100, 100);
+  pop();
 
+  push();
+  fill(255,255,255);
+  textSize(12);
+  textAlign(LEFT);
+  text(`Time ${timer}`, width-100, 100);
+  pop();
+}
 function displayPin(baseX, baseY, tipX, tipY){
   //pin body
   push();
@@ -148,4 +195,16 @@ function displayPin(baseX, baseY, tipX, tipY){
   fill(255,0,0);
   ellipse(baseX,baseY,20);
   pop();
+}
+
+function mousePressed(){
+  if(gamestate === 'menu')
+{
+  gamestate = 'game'
+}
+if(gamestate === 'gameover'){
+  gamestate= 'game'
+  score = 0
+  timer = 60
+}
 }
