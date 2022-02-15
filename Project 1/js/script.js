00d9a33616752a56fc1dfa3eb27d02c685ee0cba
Noe235 <sannoemie@outlook.com>
2022-02-15 02:21:52 -0500

    P1-prepping files

---
+++ Project 1/js/script.js
@@ -0,0 +1,39 @@
+/**
+A night at the movie
+Noemie
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+let gamestate = 'menu' //menu,gamejob,gamereal,gameover,
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
+
+function mousePressed() {
+
+}
