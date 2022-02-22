554fc8c168fd62dadc47fd1a13b1bc098747134d
Noe235 <sannoemie@outlook.com>
2022-02-22 15:56:11 -0500

    Haiku excersies done

--- activities/Haiku Generator/js/script.js
+++ activities/Haiku Generator/js/script.js
@@ -1,11 +1,92 @@
 /**
-Title of Project
-Author Name
+Haiku Generator
+Noemie
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Haiku Generator
 */
 
+// Make a synthesized voice read out the poem each time it changes or on a separate user interaction
+//Change or add to the basic transition of a fade in and out (could you make new lines appear with a typewriter effect?)
+//fix the ines at the top
 "use strict";
 
-// Code goes here
+let fiveSyllablesLines =[
+  'O, to be a tree',
+  'The cat does not know',
+  'We are all forests',
+  'You have done your best',
+  'They are all gone now',
+  ];
+
+let sevenSyllablesLines=[
+  'Say the things left unsaid',
+  "Never believe the wind's lies",
+  'The autumn stretches its legs',
+  'Nothing can satisfy you',
+  'They will not come back again',
+  ];
+
+
+let line1= random (fiveSyllablesLines);
+let line2= random (sevenSyllablesLines);
+let line3= random (fiveSyllablesLines);
+
+
+let line1P= document.getElementById('line-1');
+let line2P= document.getElementById('line-2');
+let line3P= document.getElementById('line-3');
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener('click', lineClicked);
+line2P.addEventListener('click', lineClicked);
+line3P.addEventListener('click', lineClicked);
+
+
+
+function lineClicked(event){
+  fadeOut(event.target, 1);
+
+}
+
+function fadeOut(element, opacity){
+  opacity-= 0.01;
+  element.style ['opacity'] = opacity;
+   if(opacity>0) {
+     requestAnimationFrame(function(){
+       fadeOut(element,opacity);
+     }) } else {
+   setNewLine(element);
+   fadeIn(element,opacity);
+     }
+   }
+
+function fadeIn(element,opacity){
+  opacity+= 0.01;
+element.style['opacity']= opacity;
+if (opacity <1){
+  requestAnimationFrame(function (){
+  fadeIn(element,opacity);
+});
+}
+}
+
+function setNewLine(element){
+  if (element=== line1P || element=== line3P){
+    element.innerText= random (fiveSyllablesLines);
+
+  } else if (element===line2P){
+    element.innerText= random (sevenSyllablesLines);
+
+  }
+
+}
+
+
+
+function random(array){
+  let index = Math.floor(Math.random()*array.length);
+  return array[index];
+}
