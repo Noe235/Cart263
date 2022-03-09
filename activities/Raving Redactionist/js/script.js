7de3d42d34e10b01b94f84591898f382e0239889
Noe235 <sannoemie@outlook.com>
2022-03-08 21:25:59 -0500

    E5-3New copypasta

--- activities/Raving Redactionist/js/script.js
+++ activities/Raving Redactionist/js/script.js
@@ -7,14 +7,56 @@ author, and this description to match your project!
 */
 
 "use strict";
+let currentAnswer = `Click to begin.`;
+let copypasta = undefined;
 
 
-$(`.top-secret`).on(`click`,redact);
+//hide and chose a copypasta
+$(`.glasses`).hide()
+$(`.justdoit`).hide()
+$(`.niagarafalls`).hide()
+$(`.minecraft`).hide()
+let d = Math.floor(Math.random() * 4);
+if (d === 0) {
+  $(`.glasses`).show()
+}
+if (d === 1) {
+  $(`.justdoit`).show()
+}
+if (d === 2) {
+  $(`.niagarafalls`).show()
+}
+if (d === 3) {
+  $(`.minecraft`).show()
+}
 
 
-setInterval(revelation,500);
 
-function redact(event){
+
+$(`.top-secret`).on(`click`, redact);
+
+
+setInterval(revelation, 500);
+currentAnswer = guessCopyPasta;
+
+
+
+// Is annyang available?
+if (annyang) {
+  // Create the guessing command
+  let commands = {
+    'I think it is *copypasta': guessCopyPasta
+
+  };
+  // Setup annyang and start
+  annyang.addCommands(commands);
+  annyang.start();
+
+}
+
+
+
+function redact(event) {
 
   $(this).removeClass(`redacted`);
   $(this).addClass(`revealed`);
@@ -25,10 +67,15 @@ function revelation() {
   $(`.revealed`).each(attemptReveal);
 }
 
-function attemptReveal(){
+function attemptReveal() {
   let r = Math.random();
-  if (r<0.1){
+  if (r < 0.05) {
     $(this).removeClass(`revealed`);
     $(this).addClass(`redacted`);
   }
+}
+
+function guessCopyPasta(copypasta) {
+  // Convert the guess to lowercase to match the answer format
+  currentAnswer = copypasta.toLowerCase();
 }
