359a28fb8a173ce37fbdca44accafd0f1a1d4f0a
Noe235 <90280480+Noe235@users.noreply.github.com>
2022-03-15 17:39:29 -0400

    E6-draggable letters

--- activities/Code_Taker/js/script.js
+++ activities/Code_Taker/js/script.js
@@ -11,15 +11,44 @@ author, and this description to match your project!
 // Code goes here
 let rightAnswer = `Ice`;
 
-// $(`.secret`).on(`click`,function(event) {
-//   $(this).addClass(`found`,500);
-//
-// });
-//
-// $(.`found`).on(`click`,function(event){
-//   $(this).draggable()
-// });
-$(`#solved-dialog`).dialog({
+
+let d = Math.floor(Math.random() * 2);
+if (d === 0) {
+  $(`.cat`).show()
+  $(`.penguin`).hide()
+  $(`.elephant`).hide()
+  $(`.owl`).hide()
+  rightAnswer = `cat`
+}
+if (d === 1) {
+   $(`.cat`).hide()
+  $(`.penguin`).show()
+  $(`.elephant`).hide()
+  $(`.owl`).hide()
+    rightAnswer = `penguin`
+}
+if (d === 2) {
+ $(`.cat`).hide()
+  $(`.penguin`).hide()
+  $(`.elephant`).show()
+  $(`.owl`).hide()
+    rightAnswer = `elephant`
+}
+if (d === 3) {
+   $(`.cat`).hide()
+  $(`.penguin`).hide()
+  $(`.elephant`).hide()
+  $(`.owl`).show()
+    rightAnswer = `owl`
+}
+
+
+
+
+
+
+
+$(`#solved-dialog-good`).dialog({
   autoOpen: false,
   buttons: {
     "Yeah!": function(){
@@ -29,12 +58,30 @@ $(`#solved-dialog`).dialog({
   }
 });
 
-$(`.secret`).one(`mouseover`,function(event) {
-  $(this).addClass(`found`,500);
-  $(this).draggable({
+// $(`#solved-dialog-bad`).dialog({
+//   autoOpen: false,
+//   buttons: {
+//     "Yeah!": function(){
+//       $(this).dialog(`close`);
+//
+//     }
+//   }
+// });
+
+$(`.secret`).on(`click`,function(event){
+  console.log(`draggin`);
+  $(`.secret`).draggable({
   helper:"clone"
-  });
 });
+  });
+
+$(`.secret`).on(`click`,function(event) {
+  $(this).addClass(`found`,500);
+  // $(this).removeClass(`found`);
+  // $(this).removeClass(`secret`);
+  });
+
+
 
 $(`#answer`).droppable({
   drop: function (event,ui){
@@ -44,11 +91,20 @@ $(`#answer`).droppable({
     ui.draggable.removeClass(`found`);
     // ui.draggable.off(`click`);
 
-    //check if they got it
-    if ($(this).text() === rightAnswer){
-      $(`#solved-dialog`).dialog(`open`);
 
     }
-  }
+
 
 })
+
+// function guess(){
+//   //check if they got it
+//   if ($(`#answer`).text() === rightAnswer){
+//     $(`#solved-dialog-good`).dialog(`open`);
+//
+// } else (){
+//     $(`#solved-dialog-bad`).dialog(`open`);
+// }
+
+
+// }
