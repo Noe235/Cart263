f851abdc72b5bddf474e209f7220195bbe614672
Noe235 <sannoemie@outlook.com>
2022-03-22 17:25:10 -0400

    P2-level 1 and 2,menu+gameover

--- Project 2/js/script.js
+++ Project 2/js/script.js
@@ -8,37 +8,70 @@ Noemie
 
 // Code goes here
 
+//variables
+let lives = 3;
+let current_level = 1;
 
+$(`#lives`).text(lives);
 //levels to hide
 $(`.level1`).hide();
+$(`.level2`).hide();
+$(`.level3`).hide();
 
 
+// menu
 //play button
 $(`.play`).on(`click`, function (event) {
-  $(`.Tittle`).fadeOut(1000);
-  $(this).fadeOut(1000, level_1);
+  $(`#lives_box`).fadeIn(1000);
+  $(`#Menu`).fadeOut(1000, level_1);
+
+
 })
 
-//wrong answer
+
+
+// In game
+//wrong answer in game
 $(`.wrong_answer`).on(`click`, function (event) {
-
-
-
-
+  $(`#X`).show();
+  setTimeout(function () {
+    $(`#X`).hide();
+    lives--;
+    $(`#lives`).text(lives);
+    if (lives <= 0) {
+      $(`#gameover`).show();
+      $(`.level1`).hide();
+    }
+  }, 2000);
 
 })
 
+//good answer in game
+$(`.good_answer`).on(`click`, function (event) {
+  $(`.level${current_level}`).hide();
+  current_level++
+  $(`.level${current_level}`).show();
+})
 
-function wrongAnswer() {
+// gameover
+//try again
+$(`#tryagain`).on(`click`, function (event) {
+  $(`#gameover`).hide();
+  $(`#Menu`).show();
+  $(`#lives_box`).hide();
+  lives = 3;
+  $(`#lives`).text(lives);
+})
 
-}
-
+// other funcions
 function ui() {
   $(`.Tittle`).fadeIn(2000);
   $(`.Tittle`).addClass(`game_ui`);
+  $
   // $(`.answer_space`).show();
 }
 
+// levels functions
 function level_1() {
   $(`.level1`).fadeIn(1000);
 }
