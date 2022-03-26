bd32d2183fa5990aa12eeb5527dd56ce9700757a
Noe235 <sannoemie@outlook.com>
2022-03-26 02:27:45 -0400

    E6-added different sprite

--- activities/desperatllyseekingsadness/js/Boot.js
+++ activities/desperatllyseekingsadness/js/Boot.js
@@ -21,9 +21,22 @@ class Boot extends Phaser.Scene {
   */
   preload() {
     // Load images
+
     this.load.image(`avatar`, `assets/images/avatar.png`);
-    this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
-    this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);
+    // this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
+    // this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);
+    // this.load.image(`ee`, `assets/images/ee.webp`);
+    // this.load.image(`sh`, `assets/images/sh.webp`);
+    let chance = Math.floor(Math.random() * 2);
+    console.log(chance);
+    if (chance === 1) {
+      this.load.image(`thumbs-down`, `assets/images/heart,red.png`);
+      this.load.image(`thumbs-up`, `assets/images/heart,pink.png`);
+    } else {
+      this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
+      this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);
+    }
+
     this.load.image(`wall`, `assets/images/brick_1f9f1.png`);
 
     //load sound
